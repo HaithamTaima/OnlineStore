938a0f147fa365014fb8ca929b47fde1acfb75b9
HaithamTaima <haithamsrk@gmail.com>
2022-08-02 10:37:49 +0300

    commit

--- resources/views/partial/backend/sidebar.blade.php
+++ resources/views/partial/backend/sidebar.blade.php
@@ -2,7 +2,7 @@
     $current_page = \Route::currentRouteName();
 @endphp
 
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion " id="accordionSidebar">
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('admin.index') }}">
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
